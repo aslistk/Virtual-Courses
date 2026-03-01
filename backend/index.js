c85dbea09882117762a72fc36bff66fa33fa5b83
Sarthak Sood <soodsarthak14@gmail.com>
2026-03-01 19:01:49 +0530

    Update CORS origin to new Vercel frontend URL

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://virtual-courses-p755.vercel.app"],
+    origin: ["http://localhost:5173", "https://virtual-courses-psi.vercel.app"],
     credentials: true,
   }),
 );
